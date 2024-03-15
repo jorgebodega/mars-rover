5b141cc6854ee2a564cf1714086fdc6351959ec8
Viren Dhanwani <viren@acidtango.com>
2024-03-15 16:01:24 +0000

    test: should move twice y position

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -21,3 +21,10 @@ def test_should_move_y_axis() -> None:
     result = mars_rover.execute("M")
 
     assert result == "0:1:N"
+
+def test_should_move_y_axis_twice() -> None:
+    mars_rover = MarsRover()
+
+    result = mars_rover.execute("MM")
+
+    assert result == "0:2:N"
